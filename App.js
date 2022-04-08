c59ba71557d5dbda97479f8b7f4064dd7d610690
kabukideto <87684925+kabukideto@users.noreply.github.com>
2022-04-08 07:46:42 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ThongTinKhaiBao from './components/ThongTinKhaiBao';
+
+
+function App() {
+  return (
+    <div className="App">
+         
+          <ThongTinKhaiBao></ThongTinKhaiBao>
+      
+   
+    </div>
+  );
+}
+
+export default App;
